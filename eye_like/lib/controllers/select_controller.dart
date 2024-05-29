cb87bf1afdba898f45f6be175027a003d6a02e4e
jiin <moonjiin98@gmail.com>
2024-05-29 10:18:09 +0900

    fix: isSelected Map 자료 추가로 에러 해결

--- eye_like/lib/controllers/select_controller.dart
+++ eye_like/lib/controllers/select_controller.dart
@@ -2,7 +2,13 @@ import 'package:get/get.dart';
 import 'package:eye_like/components/select_button_widget.dart';
 
 class SelectController extends GetxController {
-  final RxMap<String, SelectButtonType> isSelected = {  // Map은 key: value형태의 자료형
+  final RxMap<String, SelectButtonType> isSelected = {
+    // Map은 key: value형태의 자료형
+    '당뇨병': SelectButtonType.type2,
+    '고혈압': SelectButtonType.type2,
+    '비만': SelectButtonType.type2,
+    '암': SelectButtonType.type2,
+    '뇌졸증': SelectButtonType.type2,
     '난류': SelectButtonType.type2,
     '우유': SelectButtonType.type2,
     '메밀': SelectButtonType.type2,
@@ -24,7 +30,10 @@ class SelectController extends GetxController {
     '잣': SelectButtonType.type2,
   }.obs;
 
-  void toggleIsSelected(String name) {  // 버튼이 눌려지면, key값을 받아오고, 해당 value인 type을 확인한 후, type 변환
-    isSelected[name] = isSelected[name] == SelectButtonType.type2 ? SelectButtonType.type1 : SelectButtonType.type2;
+  void toggleIsSelected(String name) {
+    // 버튼이 눌려지면, key값을 받아오고, 해당 value인 type을 확인한 후, type 변환
+    isSelected[name] = isSelected[name] == SelectButtonType.type2
+        ? SelectButtonType.type1
+        : SelectButtonType.type2;
   }
 }
