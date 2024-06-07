bc08be6acbf57c1bbcd0999ecc5f1af1b5cde61a
jiin <moonjiin98@gmail.com>
2024-06-07 22:03:40 +0900

    feat: 정규표현식 적용 및 tts 기능 추가

--- eye_like/lib/pages/basic_first.dart
+++ eye_like/lib/pages/basic_first.dart
@@ -4,6 +4,7 @@ import 'package:eye_like/controllers/image_controller.dart';
 import 'package:eye_like/controllers/text_recognition_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 import 'package:get/get.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -19,6 +20,7 @@ class _BasicFirstState extends State<BasicFirst> {
   String extractedText = '';
   final TextRecognitionService _textRecognitionService =
       TextRecognitionService();
+  FlutterTts flutterTts = FlutterTts();
 
   @override
   void initState() {
@@ -28,21 +30,38 @@ class _BasicFirstState extends State<BasicFirst> {
 
   Future<void> extractText() async {
     //텍스트 추출
-    ByteData data = await rootBundle.load('assets/images/food_label_5.jpeg');
+    ByteData data = await rootBundle.load('assets/images/food_label_7.jpeg');
     Uint8List bytes = data.buffer.asUint8List();
     Directory tempDir = await getTemporaryDirectory();
     File imageFile =
-        await File('${tempDir.path}/food_label_5.jpeg').writeAsBytes(bytes);
+        await File('${tempDir.path}/food_label_7.jpeg').writeAsBytes(bytes);
 
     try {
       String text =
           await _textRecognitionService.recognizeTextFromImage(imageFile);
+
+      // 정규표현식 패턴
+      RegExp regExp = RegExp(r'(나트륨|탄수화물|지방)\s*(\d+)\s*(mg|g)');
+      // 매칭 결과
+      Iterable<RegExpMatch> matches = regExp.allMatches(text);
+
+      // 추출된 텍스트를 하나의 문자열로 저장
+      String regexExtractedText =
+          matches.map((match) => match.group(0)).join('\n');
+
       setState(() {
-        extractedText = text;
+        extractedText = regexExtractedText;
+        _speak(extractedText);
       });
     } catch (e) {
       print('Error: $e');
     }
+  }
+
+  Future<void> _speak(String text) async {
+    await flutterTts.setLanguage('ko-KR');
+    await flutterTts.setSpeechRate(0.5); // 속도 조절 (0.0 ~ 1.0)
+    await flutterTts.speak(text);
   }
 
   Widget _previousButton() {
